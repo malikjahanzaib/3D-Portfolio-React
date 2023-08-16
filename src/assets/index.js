96923f901bfa4d7b7d678deeaa4aadcef81c940b
malikjahanzaib <jahanzaibmlk321@gmail.com>
2023-08-16 15:57:48 -0700

    Added Experience Section.
    - Implemented Vertical Timeline Component.
    - Created Experience Card Component.
    - Updated Experience Contents.

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,6 +25,8 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import CCSF from "./company/CCSF.png";
+import BMCP from "./company/BMCP.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -59,4 +61,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  CCSF,
+  BMCP
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -23,6 +23,8 @@ import {
     jobit,
     tripguide,
     threejs,
+    CCSF,
+    BMCP
   } from "../assets";
   
   export const navLinks = [
@@ -115,56 +117,35 @@ import {
   ];
   
   const experiences = [
+   
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
-      iconBg: "#383E56",
-      date: "March 2020 - April 2021",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
+      title: "Web Developer Intern",
+      company_name: "BMCP Solutions",
+      icon: BMCP,
       iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
+      date: "December 2019 - Feb 2020",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Improved the official website's Front End/UI, leading to 30% faster load time through optimization techniques and image compression.",
+        "Assisted in developing a CRM project, resulting in 20% increased sales productivity and 15% faster customer response time.",
+        "Worked in an Agile environment, reducing project delivery time by 10% through adaptive planning and regular feedback.",
+        "Optimized code and implemented Responsive Design, reducing page load sizes by 25% and boosting mobile traffic by 20%.",
+        "Maintained a 95% client satisfaction rate through effective communication and incorporating client feedback.",
       ],
     },
     {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
+      title: "Software Engineer and Cyber Security Intern",
+      company_name: "City and County of San Francisco",
+      icon: CCSF,
       iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
+      date: "May 2022 - December 2022",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
-      iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Researched and evaluated 5+ Chatbot frameworks for MyApps System, resulting in the identification of three optimal frameworks for diverse use cases.",
+        "Developed and tested proof of concept implementations, effectively resolving 60% of user queries, resulting in a 30% reduction in support ticket volumes.",
+        "Collaborated with a 6-member cross-functional team to gather requirements and optimize Chatbot performance.",
+        "Implemented NLP and machine learning libraries, resulting in a 25% improvement in chatbot understanding and response accuracy.",
+        "Conducted 5+ knowledge-sharing sessions, enhancing team-wide proficiency in chatbot development and NLP techniques.",
+        "Conducted extensive FIDO MFA - YubiKey testing, ensuring seamless authentication for over 30,000 employees, with a 20% decrease in security-related incidents.",
+        "Prepared comprehensive technical documentation, contributing to a 30% reduction in onboarding time for new team members.",
       ],
     },
   ];
