b1062d15d08a39a7892fd286538d113a64e30a94
malikjahanzaib <jahanzaibmlk321@gmail.com>
2023-08-23 13:39:51 -0700

    Viewport bug fixes for smaller mobile devices.

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -22,7 +22,7 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full h-full"
       >
         <div className='relative w-full h-[230px] '>
           <img 
